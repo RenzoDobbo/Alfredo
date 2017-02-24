a9e2a5b052b4a9d54a35dc55e1ba2c1747f2a338
RenzoDobbo <renzo_0_8@hotmail.com>
2017-02-23 23:49:58 -0300

    Primer commit
    A ver que onda

---
+++ Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Debug.Log(pos);
+
+    }
+
+    void OnMouseDown()
+    {
+
+        this.transform.position = Input.mousePosition;
+        Debug.Log("2134");
+
+    }
+
+}
---
+++ Assets/OnClick.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClick : MonoBehaviour {
+
+    public float speed = 1000;
+    Vector3 v3 = new Vector3();
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var braulio = GameObject.Find("Braulio");        
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            v3 = Input.mousePosition;
+            v3.z = 1;
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+
+            Debug.Log(v3);
+
+        }
+
+        braulio.transform.position = Vector3.MoveTowards(braulio.transform.position, v3, 0.5f);
+
+    }
+
+    void OnMouseDown()
+    {
+       
+
+    }
+}
